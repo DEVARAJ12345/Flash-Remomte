b5d238474107b21e7531354c0ea9fc5a0cac8a4c
dbatthul <dbatthul@DBATTHUL-7RJC3.partnet.cisco.com>
2021-01-30 10:17:55 +0530

    Pushing code into git

---
+++ javaTasks/src/javaTasks/First.java
@@ -0,0 +1,18 @@
+package javaTasks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+
+public class First
+{
+	
+	public static void main(String args[])
+	{
+		String s="a4b6c2";
+		
+	}
+}
+
